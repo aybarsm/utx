677e6683c9878ec7f86d816fce5fc80a9e0228c2
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2025-12-30 17:34:08 +0100

    loginctl cmd

--- app/Commands/Loginctl.php
+++ app/Commands/Loginctl.php
@@ -2,22 +2,29 @@
 
 namespace App\Commands;
 
+use App\Dtos\Loginctl\Session;
 use Tempest\Console\Console;
 use Tempest\Console\ConsoleCommand;
+use Tempest\Process\ProcessExecutor;
 use function Tempest\get;
+use function Tempest\map;
 
 final class Loginctl
 {
     public function __construct(
         private Console $console,
+        private ProcessExecutor $exec,
     ) {
     }
 
     #[ConsoleCommand(name: 'loginctl:sessions')]
     public function sessions(): void
     {
-        /** @var \App\Services\Loginctl $sessions */
-        $loginctl = get('loginctl');
-        dump($loginctl->sessions());
+//        /** @var \App\Services\Loginctl $sessions */
+//        $loginctl = get(tag: 'loginctl');
+        $sessions = map($this->exec->run('loginctl list-sessions --json=short')->output)
+            ->collection()
+            ->to(Session::class);
+        dump($sessions);
     }
 }
